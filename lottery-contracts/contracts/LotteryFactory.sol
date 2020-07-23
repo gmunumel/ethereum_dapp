db894161e8e767428ca3c7c18dc1105466a8339d
gabriel <gabrielmunumel@gmail.com>
2020-07-23 15:21:03 +0000

    Modificado service

--- lottery-contracts/contracts/LotteryFactory.sol
+++ lottery-contracts/contracts/LotteryFactory.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.26; //Lo dice en el video del día 4 a las 2:32:28
 
+
 /**
  * @title Storage
  * @dev Store & retreive value in a variable
@@ -8,8 +9,8 @@ pragma solidity ^0.4.26; //Lo dice en el video del día 4 a las 2:32:28
 contract LotteryFactory {
 
     address[] private listLotteries; // mapping no deja iterar sobre el. Por lo tanto, va a ser necesario para poder devolver el listado de TODAS las loterias.
-    
-    
+
+
     // Creamos loteria
     // Creamos la loteria de prueba y colocamos payable y no payable y funciona en ambos casos, investigar TODO
     function createLottery(uint8 _maxNumberParticipants, uint256 _participationPrice, uint256 _pot, uint256 _prize) public payable {
@@ -35,9 +36,9 @@ contract Lottery {
     uint256 private participationPrice;
     uint256 private pot;
     uint256 private prize;
-    State private status;
     address private ownerLottery;
     address private masterContract;
+    State private status;
 
     address[] private listParticipants; // lista de los participantes.
     mapping (address => uint256) private mapParticipants;  // key: address de la participante, y value: el valor de la participacion.
@@ -81,12 +82,12 @@ contract Lottery {
     }
     
     // Añadir participante de loteria
-    // Se usa el patrond e estados
+    // Se usa el patron de estados
     function addParticipant() public payable atState(State.Active) {
         
         require(mapParticipants[msg.sender] != participationPrice); // el participante no este ya en la loteria
         require(msg.value == participationPrice);                   //verificar que el participante participa con el precio de la loteria
-    
+
         
         mapParticipants[msg.sender] = participationPrice;
         listParticipants.push(msg.sender);
@@ -124,23 +125,22 @@ contract Lottery {
     
     // Sorteo de loteria.
     // Usamos el patron de estados y el patron ownable
-    function raffle() public payable atState(State.Finished) onlyOwner(){
-    
+     function raffle() public payable atState(State.Finished) onlyOwner(){
+
         require(msg.sender == ownerLottery);
-        
-        uint8 winner = random();
+
+        uint8 winner = uint8(random() % listParticipants.length);
         listParticipants[winner].transfer(prize);
-        
+      
         ownerLottery.transfer((listParticipants.length * participationPrice) - prize);
         
+    
         status = State.Terminated;
-    }
+     }
 
     function random() private view returns (uint8) {
-        LotteryFactory l = LotteryFactory(masterContract);
-        return uint8(uint(keccak256(block.timestamp, block.difficulty,l.getLotteries().length))%listParticipants.length);
+       LotteryFactory l = LotteryFactory(masterContract);
+       return uint8(keccak256(abi.encodePacked(block.timestamp, block.difficulty, l.getLotteries().length)));
     }
-
 }  
-
 
